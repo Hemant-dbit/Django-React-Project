3a34e87e061517ad2139f0b5445f75b9ee0f0368
Hemant <guptahemant350@gmail.com>
2025-02-18 21:58:13 +0530

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,24 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
 
-const api = axios.create(
-    {
-        baseURL: import.meta.env.VITE_API_URL
-    }
-)
+const apiUrl =
+  "https://58c5d4c3-aa70-4249-8a08-2bd515569797-dev.e1-us-east-azure.choreoapis.dev/django-react-project/backend/v1";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`
-        }
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-export default api
+export default api;
