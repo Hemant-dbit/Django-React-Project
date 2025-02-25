0d8b1db15e5bf5605f46f79e9cfc7fc6a32ecca0
Hemant <guptahemant350@gmail.com>
2025-02-25 22:26:21 +0530

    connected to local host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "PORT": os.getenv("DB_PORT"),
-        "HOST": os.getenv("DB_HOST"),
+        'NAME': "djangodb",
+        "USER": "postgres",
+        "PASSWORD": "Hemant123$",
+        "PORT": "5432",
+        "HOST": "localhost",
     }
 }
 
